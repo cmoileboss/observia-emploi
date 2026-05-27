5ab2be0f7e033a0d911db518081bd406c855840a
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-05-27 15:00:17 +0200

    Changement chemins dans extractor csv

--- csv_extractor.py
+++ csv_extractor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-corr = pd.read_csv("resources/correspondance-rome-rncp-tech-6a16c0f17343f806639940.csv", sep=";")
-form = pd.read_csv("resources/entree_sortie_formation.csv", sep=";")
+corr = pd.read_csv("Ressources/correspondance-rome-rncp-tech-6a16c0f17343f806639940.csv", sep=";")
+form = pd.read_csv("Ressources/entree_sortie_formation.csv", sep=";")
 
 result = form.merge(corr[["code_rncp", "code_rome", "intitule_rome", "niveau_rncp"]], 
                     on="code_rncp", how="left")
-result.to_csv("resources/joined.csv", sep=";", index=False)
+result.to_csv("Ressources/joined.csv", sep=";", index=False)
