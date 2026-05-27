4fc441d9eb5a54c4e8cd0cd9a97a5158da04ad23
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-05-27 14:46:35 +0200

    Revert "readme requirements"
    This reverts commit 7c9d60c8a8f915dfa5845c087d36e9206bd91072.

--- csv_extractor.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-corr = pd.read_csv("resources/correspondance-rome-rncp-tech-6a16c0f17343f806639940.csv", sep=";")
-form = pd.read_csv("resources/entree_sortie_formation.csv", sep=";")
