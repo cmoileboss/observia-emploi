6cda3c296e411d35192c5bad662e5543623ec195
pedrona.guillaume <pedrona.guillaume@gmail.com>
2026-05-29 23:56:33 +0200

    modif csv_extractor

--- csv_extractor.py
+++ csv_extractor.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-<<<<<<< HEAD
 
 class CsvExtractor:
 
@@ -12,23 +11,14 @@ class CsvExtractor:
 
     def clean_data(self):
         """Nettoyage des données : suppression des colonnes inutiles, des code RNCP égaux à -1 et uniformisation des codes RNCP"""
-        self.formation = self.formation.drop(columns=["annee_mois", "type_referentiel", "code_rs", "code_certifinfo", "siret_of_contractant", "raison_sociale_of_contractant", "date_chargement"])
+        self.formation = self.formation.drop(columns=["annee_mois", "type_referentiel", "code_rs", "code_certifinfo", "siret_of_contractant", "raison_sociale_of_contractant"])
         self.formation = self.formation[self.formation["code_rncp"] != "-1"]
-        self.correspondance = self.correspondance.drop(columns=["intitule_rncp"])
         self.correspondance["code_rncp"] = self.correspondance["code_rncp"].str.replace("RNCP", "", regex=False)
 
     def merge_data(self):
         """Fusion des données formation et correspondance"""
-        self.merged = self.formation.merge(self.correspondance, on="code_rncp", how="inner")
-        self.merged = self.merged[self.merged["entrees_formation"] > 0]
+        self.merged = self.formation.merge(self.correspondance, on="code_rncp", how="left")
 
     def export(self, filepath: str):
-        """Export des données agrégées vers le dossier processed, dans le fichier merged_data.csv"""
+        """Export des données agrégées vers le dossier processed, dans le fichier merged_date.csv"""
         self.merged.to_csv(f"{filepath}", sep=";", index=False)
-=======
-corr = pd.read_csv("resources/correspondance-rome-rncp-tech-6a16c0f17343f806639940.csv", sep=";")
-form = pd.read_csv("resources/entree_sortie_formation.csv", sep=";")
-
-result = form.merge(corr[["code_rncp", "code_rome", "intitule_rome", "niveau_rncp"]], on="code_rncp", how="left")
-result.to_csv("resources/joined.csv", sep=";", index=False)
->>>>>>> main
